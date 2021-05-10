a4b3738a53f012ef98f38a07119bcd41ca653b5a
Christian Kruse <christian.kruse.bt@gmail.com>
2021-05-10 16:26:29 +0200

    simple dash layout

--- src/dash/app.py
+++ src/dash/app.py
@@ -1,29 +1,26 @@
 import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import requests
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, prevent_initial_callbacks=True)
 
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    html.H1("Dash Template"),
+    html.Button('Test API', id='test-api', n_clicks=0),
+    html.Div(id='container-api-result',
+             children='')
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(
+    dash.dependencies.Output('container-api-result', 'children'),
+    [dash.dependencies.Input('test-api', 'n_clicks')])
+def update_output(n_clicks):
+    greeting = requests.get("http://127.0.0.1:80/test/HelloWorld")
+    return html.H2(greeting)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
