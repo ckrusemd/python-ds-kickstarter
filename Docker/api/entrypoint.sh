b7a1e484b72d1f6e580d0661d3610a65430e0320
Christian Kruse <christian.kruse.bt@gmail.com>
2021-05-12 10:48:34 +0200

    changed api port to 8080 + link to api in dockercompose
    conflicted with nginx port 80

--- Docker/api/entrypoint.sh
+++ Docker/api/entrypoint.sh
@@ -3,4 +3,4 @@
 cd /app/
 /usr/local/bin/python -m pip install --upgrade pip
 pip install -r requirements.txt
-uvicorn src.api.main:app --host 0.0.0.0 --port 80
+uvicorn src.api.main:app --host 0.0.0.0 --port 8080
